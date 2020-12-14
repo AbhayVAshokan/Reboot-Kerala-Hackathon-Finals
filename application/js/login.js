6c06eea1ed89584772c5010b10efc16651baae73
misha9 <mishamethyst@gmail.com>
2020-12-14 20:41:18 +0530

    Added login files

---
+++ application/js/login.js
@@ -0,0 +1,26 @@
+function validate() {
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
+            if (username == null || username == "") {
+                alert("Please enter the username.");
+                return false;
+            }
+            if (password == null || password == "") {
+                alert("Please enter the password.");
+                return false;
+            }
+        window.location.href=" ";
+        } 
+
+function checkEmail() {
+
+    var email = document.getElementById('txtEmail').value;
+    var filter =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!filter.test(email)) {
+    alert('Please provide a valid email address!');
+    email.focus;
+    return false;
+ }
+            alert('Please check your email to reset your password.');
+}
